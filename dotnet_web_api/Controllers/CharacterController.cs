57f2a4921df6a3ae1ef91560a145ab2429feca71
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2022-04-10 16:30:14 +0100

    setup up character model

---
+++ dotnet_web_api/Controllers/CharacterController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+    }
+}
