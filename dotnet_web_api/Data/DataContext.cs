b980a7bfc324a86a768abc4f8a577730971f4401
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2022-04-12 13:27:58 +0100

    set character table on the db

---
+++ dotnet_web_api/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_web_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_web_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+        
+        public DbSet<Character> Characters { get; set; }
+
+    }
+}
