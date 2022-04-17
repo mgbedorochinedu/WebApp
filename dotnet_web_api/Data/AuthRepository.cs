635ad3eb2b970cab06cc767b92bd52ebef8ccdb4
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2022-04-17 23:29:02 +0100

    Implement user registeration

---
+++ dotnet_web_api/Data/AuthRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_web_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_web_api.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _db;
+
+        public AuthRepository(DataContext db)
+        {
+            _db = db;
+        }
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            ServiceResponse<int> response = new ServiceResponse<int>();
+
+            var checkExistingUser = await UserExists(user.Username);
+            if(checkExistingUser)
+            {
+                response.Success = false;
+                response.Message = "User already exists.";
+                return response;
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+
+            response.Data = user.Id;
+            return response;
+        }
+
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            var dbExistingUser = await _db.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower());
+            if (dbExistingUser)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+
+    }
+}
---
+++ dotnet_web_api/Data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_web_api.Models;
+
+namespace dotnet_web_api.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+
+}
